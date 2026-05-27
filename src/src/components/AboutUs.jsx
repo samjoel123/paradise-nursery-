bb1ed029a11a830582510aca06275626d06788c0
samjoel123 <samjojeorge05@gmail.com>
2026-05-28 00:34:28 +0530

    Add AboutUs component for nursery information

---
+++ src/src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>We provide beautiful plants for your home.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
